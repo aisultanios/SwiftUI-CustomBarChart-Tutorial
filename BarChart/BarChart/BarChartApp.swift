2e6351d66905f1d05756e0fbb257a33d658df8b2
Aisultan <36818367+AisultanAskarov@users.noreply.github.com>
2023-04-01 13:28:47 +0300

    Add files via upload

---
+++ BarChart/BarChart/BarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartApp.swift
+//  BarChart
+//
+//  Created by Aisultan Askarov on 1.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
